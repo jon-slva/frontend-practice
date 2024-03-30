f19197b45da01ef43209567a9cb6cfaafcbafe4c
Jonathan Silva <jonathan.silva167@gmail.com>
2024-03-29 23:13:09 -0400

    Install Vite, setup Browser Router, Create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+		<>
+			<h1>Frontend Practice</h1>
+
+			<h2>Stack</h2>
+			<ul>
+				<li><a href=""></a></li>
+			</ul>
+			<ul>
+				<li>React + Vite (with SWC)</li>
+				<li>Node.js</li>
+				<li>Express</li>
+			</ul>
+			<h2>Packages</h2>
+			<ul>
+				<li>Sass</li>
+				<li>Axios</li>
+
+			</ul>
+		</>
+	)
+}
+
+export default Home
