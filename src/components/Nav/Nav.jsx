892804eb44ee9a4685d8e06c4135ff867842b2cd
Jonathan Silva <jonathan.silva167@gmail.com>
2024-03-30 19:28:28 -0400

    Refactor navigation menu and add fade-in/fade-out animations

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,63 +1,137 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './Nav.scss'
 
 const Nav = () => {
-	return (
-		<nav className='nav-container'>
-			<ul className='nav'>
-				<li className='nav__items'>APIs</li>
-				<li className='nav__items'>
-					React Forms
-					<ul className='nav__sub-items'>
-						<li>Forms with State</li>
-						<li>Forms with Refs</li>
-						<ul className='nav__sub-sub-items'>
-							Form Libraries
-							<li>React Hook Form</li>
-							<li>Formik</li>
-							<li>Yup</li>
-							<li>React Select</li>
-							<li>React Datepicker</li>
-							<li>React Timepicker</li>
-						</ul>
+	const [menuState, setMenuState] = useState({
+		reactMenuOn: false,
+		uiMenuOn: false,
+		reactMenuFadeTransition: false,
+		uiMenuFadeTransition: false
+	})
 
-					</ul>
-				</li>
-				<li>
-					React Hooks
-					<ul className='nav__sub-items'>
-						<li>useState</li>
-						<li>useEffect</li>
-						<li>useContext</li>
-						<li>useReducer</li>
-						<li>useMemo</li>
-						<li>useRef</li>
-						<li>useCallback</li>
-						<li>useLayoutEffect</li>
-					</ul>
-				</li>
-				<li>
-					React Router
-					<ul className='nav__sub-items'>
-						<li>React Router Dom</li>
-						<li className='nav__items'>UseParams</li>
-						<li className='nav__items'>UseLocation</li>
-						<li>React Router Native</li>
-					</ul>
-				</li>
-				<li>
-					Other React Features
-					<ul className='nav__sub-items'>
-					</ul>
-				</li>
-				<li className='nav__items'>Animations</li>
-				<li className='nav__items'>Material UI</li>
-				<li className='nav__items'>Accessibility</li>
-				<li className='nav__items'>Lazy Loading</li>
-				<li className='nav__items'>Suspense</li>
-				<li>Tables</li>
-			</ul>
-		</nav>
+	let timeoutId = null;
+
+	const handleMenu = (menuName) => {
+		if (menuName === "reactMenuOn" || menuName === "uiMenuOn") {
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+			setMenuState(prevState => ({
+				...prevState,
+				[menuName]: true,
+				[menuName.replace("MenuOn", "MenuFadeTransition")]: true
+			}));
+		} else if (menuName === "reactMenuFadeTransition" || menuName === "uiMenuFadeTransition") {
+			setMenuState(prevState => ({
+				...prevState,
+				[menuName]: false
+			}));
+			timeoutId = setTimeout(() => {
+				setMenuState(prevState => ({
+					...prevState,
+					[menuName.replace("MenuFadeTransition", "MenuOn")]: false
+				}));
+			}, 250);
+		}
+	}
+
+	return (
+		<>
+			<nav className='nav-container'>
+				<ul className='nav'>
+					<li className='nav__item'>
+						<a href="" className='nav__item-link'>
+							APIs
+						</a>
+					</li>
+					<li className='nav__item' onMouseEnter={() => handleMenu("reactMenuOn")} onMouseLeave={() => handleMenu("reactMenuFadeTransition")}>
+						<a href="" className='nav__item-link'>
+							React
+						</a>
+						{menuState.reactMenuOn && (
+							<ul className={`react-submenu ${menuState.reactMenuFadeTransition ? "fade-in" : "fade-out"}`} onMouseEnter={() => handleMenu("reactMenuOn")} onMouseLeave={() => handleMenu("reactMenuFadeTransition")}>
+								<li className='react-submenu__header'>
+									React Forms
+									<ul className='react-submenu__nav'>
+										<li className='react-submenu__nav--item'>Forms with State</li>
+										<li className='react-submenu__nav--item'>Forms with Refs</li>
+										<li className='react-submenu__header2'>
+											Form Libraries
+											<ul className='react-submenu-2__nav'>
+												<li className='react-submenu__nav--item'>React Hook Form</li>
+												<li className='react-submenu__nav--item'>Formik</li>
+												<li className='react-submenu__nav--item'>Yup</li>
+												<li className='react-submenu__nav--item'>React Select</li>
+												<li className='react-submenu__nav--item'>React Datepicker</li>
+												<li className='react-submenu__nav--item'>React Timepicker</li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li className='react-submenu__header'>
+									React Hooks
+									<ul className='react-submenu__nav'>
+										<li className='react-submenu__nav--item'>useState</li>
+										<li className='react-submenu__nav--item'>useEffect</li>
+										<li className='react-submenu__nav--item'>useContext</li>
+										<li className='react-submenu__nav--item'>useReducer</li>
+										<li className='react-submenu__nav--item'>useMemo</li>
+										<li className='react-submenu__nav--item'>useRef</li>
+										<li className='react-submenu__nav--item'>useCallback</li>
+										<li className='react-submenu__nav--item'>useLayoutEffect</li>
+									</ul>
+								</li>
+								<li className='react-submenu__header'>
+									React Router
+									<ul className='react-submenu__nav'>
+										<li className='react-submenu__nav--item'>React Router Dom</li>
+										<li className='react-submenu__nav--item'>UseParams</li>
+										<li className='react-submenu__nav--item'>UseLocation</li>
+										<li className='react-submenu__nav--item'>React Router Native</li>
+									</ul>
+								</li>
+								<li className='react-submenu__header'>
+									Loading
+									<ul className='react-submenu__nav'>
+										<li className='react-submenu__nav--item'>Suspense</li>
+										<li className='react-submenu__nav--item'>Lazy Loading</li>
+									</ul>
+								</li>
+							</ul>
+						)}
+					</li>
+					<li className='nav__item'>
+						<a href="" className='nav__item-link'>
+							Animations
+						</a>
+					</li>
+					<li className='nav__item' onMouseEnter={() => handleMenu("uiMenuOn")} onMouseLeave={() => handleMenu("uiMenuFadeTransition")}>
+						<a href="" className='nav__item-link'>
+							UI Libraries
+						</a>
+						{menuState.uiMenuOn && (
+							<ul className={`ui-submenu ${menuState.uiMenuFadeTransition ? "fade-in" : "fade-out"}`} onMouseEnter={() => handleMenu("uiMenuOn")} onMouseLeave={() => handleMenu("uiMenuFadeTransition")}>
+								<li className='ui-submenu__item'>Material UI</li>
+								<li className='ui-submenu__item'>Ant Design</li>
+								<li className='ui-submenu__item'>Chakra UI</li>
+								<li className='ui-submenu__item'>React Bootstrap</li>
+								<li className='ui-submenu__item'>Reactstrap</li>
+							</ul>
+						)}
+					</li>
+					<li className='nav__item'>
+						<a href="" className='nav__item-link'>
+							Accessibility
+						</a>
+					</li>
+					<li className='nav__item'>
+						<a href="" className='nav__item-link'>
+							Tables
+						</a>
+					</li>
+				</ul>
+			</nav >
+		</>
 	)
 }
 
