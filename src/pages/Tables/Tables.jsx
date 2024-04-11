e7387d9facb0b2d43339f3e940cfcdb5c22d876c
Jonathan Silva <jonathan.silva167@gmail.com>
2024-04-10 21:00:04 -0400

    Add react-hook-form dependency and update Nav and Tables components

--- src/pages/Tables/Tables.jsx
+++ src/pages/Tables/Tables.jsx
@@ -1,14 +1,63 @@
 import './Tables.scss'
-import React from 'react'
+import { useState } from 'react'
 import tableData from '../../data/tables_data.json'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark, coy, funky } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import TableList from '../../components/TableList/TableList';
+import TableForm from '../../components/TableForm/TableForm';
 
 const Tables = () => {
-	const [users, setUsers] = React.useState(tableData);
+	const [users, setUsers] = useState(tableData);
+	const [newUser, setNewUser] = useState({ // creates a new user template to start with. This will be updated as the user types in the form. 
+		name: '', // these will also serve as the default starting data (value)
+		email: '',
+		phone: '',
+		dob: '',
+	});
+	const [errors, setErrors] = useState({ name: '', email: '', phone: '', dob: '' }); // will hold the errors for each field
 
 
+	const validateForm = (value, type) => {
+		//something about name, email, phone, dob
+		if (type === 'name') {
+			return value.trim() === "" ? "Name is required" : ""; // LIVE trim removes whitespace and if empty, renders the error with this text below the input
+		} else if (type === 'email') {
+			const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+			return emailRegex.test(value) ? "" : "Invalid email address"; // LIVE basic regex validation of email.
+		} else {
+			const phoneRegex = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/;
+			return phoneRegex.test(value) || value === "" ? "" : "Invalid phone number"; // LIVE basic regex phone number
+		}
+
+
+	}
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		let validationError = '';
+
+		switch (
+		name
+		) {
+			case "name":
+				validationError = validateForm(value, 'name');
+				break;
+			case "email":
+				validationError = validateForm(value, 'email');
+				break;
+			case "phone":
+				validationError = validateForm(value, 'phone');
+				break;
+			default:
+				break;
+		}
+
+		console.log(validationError);
+
+		setErrors({ ...errors, [name]: validationError })
+		setNewUser({ ...newUser, [name]: value });
+		console.log(errors);
+	}
 
 	const addUser = (user) => {
 		// something that adds it to state
@@ -17,6 +66,11 @@ const Tables = () => {
 	const deleteEntry = (index) => {
 		// something that removes it from state
 	}
+
+	const addButtonDisabled =
+		newUser.name === '' ||
+		newUser.email === '' ||
+		newUser.phone === '';
 
 	return (
 		<main>
@@ -30,7 +84,7 @@ const Tables = () => {
 				<ul>
 					<li>Form must include validation</li>
 					<li>Form must include a submit button</li>
-					<li>Form must include a calendar</li>
+					<li>Form must include a calendar✔️</li>
 					<li>Form must include delete functionality</li>
 				</ul>
 			</ol>
@@ -54,6 +108,13 @@ const Tables = () => {
 				</thead>
 				<TableList users={users} onDelete={deleteEntry} />
 
+				<TableForm
+					onSubmit={addUser}
+					onChange={handleInputChange}
+					newUser={newUser}
+					addButtonDisabled={addButtonDisabled}
+				/>
+
 			</table>
 
 			{/* HTML Table Tags
@@ -67,7 +128,19 @@ Tag	Description
 <col>	Specifies column properties for each column within a <colgroup> element
 <thead>	Groups the header content in a table
 <tbody>	Groups the body content in a table
-<tfoot>	Groups the footer content in a table */}
+<tfoot>	Groups the footer content in a table 
+
+type: Specifies the type of input element to display. The default type is "text". Other possible types include "password", "checkbox", "radio", "submit", etc.
+name: Used to reference the form data after a form is submitted.
+value: Defines the initial (default) value of the input field.
+placeholder: Specifies a short hint that describes the expected value of an input field.
+required: Specifies that an input field must be filled out before submitting the form.
+disabled: Specifies that an input field should be disabled.
+readonly: Specifies that an input field is read-only (cannot be changed).
+id: Specifies a unique id for the input field. It's often used in conjunction with the <label>'s for attribute to associate the label with the input field.
+
+
+*/}
 
 
 		</main>
